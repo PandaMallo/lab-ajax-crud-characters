4e9914e608874f62d2c19a09c871509136402994
Alejandro Mallo <mallomanzi@gmail.com>
2021-09-08 09:13:21 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,19 @@
 class APIHandler {
   constructor (baseUrl) {
+
     this.BASE_URL = baseUrl;
+    this.app = axios.create({
+      baseURL: 'https://minions-api.herokuapp.com/'
+  })
   }
 
-  getFullList () {
+  getFullList = () => this.app.get('/characters')
 
-  }
+  getOneRegister = id => this.app.get(`/characters/${id}`)
 
-  getOneRegister () {
+  createOneRegister = info => this.app.post('/characters', info)
 
-  }
+  updateOneRegister = (id, info) => this.app.put(`/characters/${id}`, info)
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister = (id) => this.app.delete(`/characters/${id}`)
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,121 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+//LISTADO
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI
+    .getFullList()
+    .then(respose =>{
+      let text = ''
+      respose.data.forEach(ele => text +=
+        `<div class="character-info">
+        <div class="name">Name: ${ele.name}</div>
+        <div class="occupation">Occupation: ${ele.occupation}</div>
+        <div class="cartoon">Is acartoon: ${ele.cartoon}</div>
+        <div class="weapon">Weapon: ${ele.weapon}</div>
+      </div>`)
+      document.querySelector('.characters-container').innerHTML = text  
+      })
+      .catch(err => console.error('ERROR', err))
+    })
+    
 
-  });
-
+//ELEMENTO POR ID
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    const id = document.querySelector('#one').value
+    charactersAPI
+   .getOneRegister(id)
+   .then(response => {
+    const { id, name, occupation, cartoon, weapon } = response.data
+    
+    let text = 
+    `<div class="character-info">
+    <div class="name">Id: ${id}</div>
+    <div class="name">Name: ${name}</div>
+    <div class="occupation">Occupation: ${occupation}</div>
+    <div class="cartoon">Is acartoon: ${cartoon}</div>
+    <div class="weapon">Weapon: ${weapon}</div>
+    </div>`
+    document.querySelector('.characters-container').innerHTML = text  
+   })
+   .catch(err => console.error('ERROR', err))
+  })
+  
 
-  });
-
+//ELIMINAR POR ID
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    const id = document.querySelector('#del-one').value
+    charactersAPI
+    .deleteOneRegister(id)
+    .then(green =>{
+      if(green.data){
+      document.querySelector('#delete-one').classList.add('active')
+      setTimeout(() => document.querySelector('#delete-one').classList.remove('active'),5000)
+      }else {
+        document.querySelector('#delete-one').classList.add('des-active')
+      setTimeout(() => document.querySelector('#delete-one').classList.remove('des-active'),5000)
+      }
+      document.querySelector('#del-one').value = ''
+    })
+    .catch(err => console.error('ERROR', err)) 
+  })
 
-  });
-
+//EDITAR
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const inputs = document.querySelectorAll('#edit-character-form input')
+    const id = inputs[0].value
+    const character = {
+      name: inputs[1].value,
+      occupation: inputs[2].value,
+      cartoon: inputs[4].value,
+      weapon: inputs[3].value
+    }
+    console.log(character);
 
+    charactersAPI
+    .updateOneRegister(id,character)
+    .then(green => {
+      document.querySelector('#edit-character-form').reset()
+      if(green.data){
+        document.querySelector('#send-up').classList.add('active')
+        setTimeout(() => document.querySelector('#send-up').classList.remove('active'),5000)
+        }else {
+          document.querySelector('#send-up').classList.add('des-active')
+        setTimeout(() => document.querySelector('#send-up').classList.remove('des-active'),5000)
+        }
+      })
+      .catch(err => console.error('ERROR', err)) 
+    })
   });
 
+
+//CREAR NUEVO
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
 
-  });
-});
+    const inputs = document.querySelectorAll('#new-character-form input')
+
+    const character = {
+        name: inputs[0].value,
+        occupation: inputs[1].value,
+        cartoon: inputs[2].value,
+        weapon: inputs[3].value
+    }
+console.log(character);
+    charactersAPI
+    .createOneRegister(character)
+    .then(green => {
+      document.querySelector('#new-character-form').reset()
+      if(green.data){
+        document.querySelector('#send-data').classList.add('active')
+        setTimeout(() => document.querySelector('#send-data').classList.remove('active'),5000)
+        }else {
+          document.querySelector('#send-data').classList.add('des-active')
+        setTimeout(() => document.querySelector('#send-data').classList.remove('des-active'),5000)
+        }
+      })
+      .catch(err => console.error('ERROR', err)) 
+    })
+  
+
